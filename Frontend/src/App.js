598eba23332400ef3be9ae3a242e3e7a1dad55e0
Sourabh Bhosale <sbhosale28802@gmail.com>
2022-08-04 23:06:52 +0530

    first commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Radio from './Components/Radio';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    </>
+  );
+}
+
+export default App;
