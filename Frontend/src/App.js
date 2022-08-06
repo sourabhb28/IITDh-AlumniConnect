70534cc9541d9e5fd6019164c62ec8251968021e
Sourabh Bhosale <sbhosale28802@gmail.com>
2022-08-07 00:05:58 +0530

    navbars added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Navbar from './Components/Navbar';
+import Navbar1 from './Components/Navbar1';
+import Navbar2 from './Components/Navbar2';
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar1/>
+      <Navbar2/>
     </>
   );
 }
