f1cd8f97c9cb0c40da9a39d215f5e2c0a0be39cd
Sourabh Bhosale <sbhosale28802@gmail.com>
2022-08-10 09:36:08 +0530

    Radio buttons added
    Added functionality of varing different dropdown options according to the radio button selected

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Navbar1 from './Components/Navbar1';
+// import Navbar1 from './Components/Navbar1';
 import Navbar2 from './Components/Navbar2';
+import Radio from './Components/Radio';
 
 function App() {
   return (
     <>
-      <Navbar1/>
+      {/* <Navbar1/> */}
       <Navbar2/>
+      <Radio/>
     </>
   );
 }
